cad0f8feafd8b9204d4929a895fb9439724180b9
Gaston <gaston010.gv@gmail.com>
2023-03-01 12:48:56 -0300

    doctsring

--- Ej1.py
+++ Ej1.py
@@ -10,7 +10,14 @@ from tkinter import ttk
 import datetime as dt
 
 class Ej1(tk.Tk):
+    """_summary_
+
+    Args:
+        tk (tk): Clase de un boton de la GUI
+    """    
     def __init__(self):
+        """Datos de la GUI y boton fecha de hoy
+        """        
         super().__init__()
         self.title("Ejercicio 1")
         self.geometry("300x300")
@@ -19,6 +26,8 @@ class Ej1(tk.Tk):
         self.mainloop()
 
     def mostrar_fecha(self):
+        """Muestra la fecha actual mediante el import datetime al momento de usar el boton fecha de hoy de la GUI
+        """        
         print("Fecha del día", dt.datetime.now())
         
 
